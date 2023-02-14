608a28f454f63d8a5dc30b6a54752dfc78311a7b
Willian Pinho <kps_wpinho@uolinc.com>
2023-02-14 11:21:03 -0300

    :tada: Initialize Project

--- OnlineSchool/LessonsView.swift
+++ OnlineSchool/LessonsView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LessonsView.swift
 //  OnlineSchool
 //
 //  Created by Willian Junior Peres de Pinho on 14/02/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LessonsView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +21,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LessonsView()
     }
 }
