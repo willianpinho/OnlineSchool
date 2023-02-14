71441102abcf2c48c577dd0210af0fea257e8ea4
Willian Pinho <kps_wpinho@uolinc.com>
2023-02-14 11:14:07 -0300

    Initial Commit

---
+++ OnlineSchool/OnlineSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineSchoolApp.swift
+//  OnlineSchool
+//
+//  Created by Willian Junior Peres de Pinho on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct OnlineSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
